839ce8d0363ba2ddf5cbf5fc37f0a97754c8cb72
muhammadirzza <muhammadirzza@gmail.com>
2020-02-24 19:45:31 +0700

    tugasday6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state ={
+    murid:[
+    ],
+    inputnama:createRef(),
+    inputusia:''
+  }
+
+  componentDidMount(){
+    var isidata=[
+      {
+        nama:'angga',
+        umur:21
+      }, 
+      {
+        nama:'sofia',
+        umur:22
+      },
+      {
+        nama:'putri',
+        umur:23
+      }
+    ]
+    this.setState({murid:isidata})
+  }
+
+  componentDidUpdate(){
+    console.log('ini did update')
+  }
+  
+  capitalfirst=(input)=>{
+    const upper = input.charAt(0).toUpperCase() + input.substring(1)
+    return upper
+  }
+
+
+  rendernamamurid=()=>{
+      return this.state.murid.map((val,index)=>{
+        return (
+        // <table>
+          
+        //   <tbody>
+            <tr>
+              <td>{index+1}</td>
+              <td>{this.capitalfirst(val.nama)}</td>
+              <td>{val.umur}</td>
+              <td>Action</td>
+            </tr>
+          // </tbody>
+        // </table>
+      )
+    })
+  }
+
+  onAddButtonClick=()=>{
+    var nama=this.state.inputnama.current.value//pake createref
+    var usia=parseInt(this.state.inputusia) //onchange
+    var muridbaru=this.state.murid
+    muridbaru.push({nama:nama,umur:usia})
+    this.state.inputnama.current.value=''
+    this.setState({murid:muridbaru,inputusia:''})
+  }
+  
+  render() {
+    if(this.state.murid.length===0){
+      return (
+        <div>
+          Loading
+        </div>
+      )
+    }
+    return (
+      <div className='tengah' style={{height: '99vh'}}>
+        <table>
+          <thead>
+              <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Usia</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+                {
+                  this.rendernamamurid()
+                }
+            </tbody>
+          <tfoot>
+            <tr>
+              <td></td>
+              <td>
+                <input type='text' className='mb-2' placeholder='masukkan nama' ref={this.state.inputnama}/>
+              </td>
+              <td>
+                <input 
+                  type='number' 
+                  placeholder='masukkan usia' 
+                  value={this.state.inputusia} 
+                  onChange={(event)=>this.setState({inputusia:event.target.value})} />
+              </td>
+              <td>
+                <button onClick={this.onAddButtonClick} className='btn btn-primary'>
+                  Add
+                </button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
+
